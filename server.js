fce5d9f463428f9f9ba1ee0c0b6ba8bbf28a6ccc
Daler <khabibov_dk@outlook.com>
2016-11-17 22:18:16 +0600

    SIS Project test

--- server.js
+++ server.js
@@ -14,21 +14,29 @@ var middleware = {
 
 app.use(middleware.logger);
 
-app.get('/', function (req, res){
-	res.send("Hello Express");
+/*app.get('/', function (req, res){
+	
+	res.send(express.static(__dirname + '/site'));
+});*/
+
+app.get('/', function(req, res) {
+	app.use(express.static(__dirname + '/templatemo_172_mini'));
+    res.sendfile('templatemo_172_mini/index.html', {root: __dirname })
 });
 
 
+//app.use('/public', public);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
 	res.send('About us');
 });
 
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 
 app.listen(PORT, function(){
 	console.log("....local server successfully launched on port <"+PORT+">");
 });
+
 
