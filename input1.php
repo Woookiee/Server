9618336391c83d75b12fe0baabb2f445fd8b12cf
Daler <khabibov_dk@outlook.com>
2016-11-17 23:43:40 +0600

    modify php

---
+++ input1.php
@@ -0,0 +1,7 @@
+<?$name = $_POST['submit'];
+$browser = $_POST['browser']; 
+$comment = $_POST['comment'];
+?>
+<p><b>Ваше имя: </b><?=$name?></p>
+<p><b>Каким браузером в основном пользуетесь: </b><?=$browser?></p>
+<p><b>Комментарий: </b><?=$comment?></p>
