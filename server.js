810f8b9e2866c7be3b0f73c1bb889fdb728191fb
Daler <khabibov_dk@outlook.com>
2016-11-20 17:47:33 +0600

    Testing POST

--- server.js
+++ server.js
@@ -1,51 +1,64 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
 var PORT = process.env.PORT || 3000;
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req,res,next){
-		console.log(req.method + ' ' + req.originalUrl);
-		next();
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.send('ToDo API Root');
+});
+
+// GET /todos
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
 	}
-};
-
-app.use(middleware.logger);
-
-/*app.get('/', function (req, res){
-	
-	res.send(express.static(__dirname + '/site'));
-});*/
-
-app.get('/', function(req, res) {
-	app.use(express.static(__dirname + '/site3'));
-    res.sendfile('site3/index.html', {root: __dirname })
 });
 
-app.use(bodyParser);
+// POST /todos
 
-app.post('/', function(req, res){
-    console.log(req.body.foo);
-    res.send('ok');
+app.post('/todos', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
+app.delete('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-
-//app.use('/public', public);
-
-app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About us');
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todo found with that id"});
+	}else{
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
-
-
-//app.use(express.static(__dirname + '/public'));
-
 
 app.listen(PORT, function(){
-	console.log("....local server successfully launched on port <"+PORT+">");
+	console.log('Express listening on port ' + PORT + '!');
 });
-
-
